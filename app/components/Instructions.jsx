79379127f71011cf95880b1e620506b110843fcd
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-09-12 16:18:53 -0700

    Worked on Instructions Component

--- app/components/Instructions.jsx
+++ app/components/Instructions.jsx
@@ -1,22 +1,36 @@
 import { List, ListItem } from 'material-ui/List';
+// import CheckCircle from 'material-ui/svg-icons/action/check-circle';
 import React from 'react';
 
 const Instructions = React.createClass({
   render() {
+    // let i = 0;
+    //
+    // const initial = <div
+    //   className="instructionCounter"
+    //   style={{ display: 'flex' }}
+    // >
+    //   <div>{i + 1}</div>
+    // </div>;
+    //
+    // const completed = <CheckCircle />
+
     return <List>
-      {this.props.instructions.map((instruction, index) =>
-        <ListItem
+      {this.props.instructions.map((instruction, index) => {
+        // i = index;
+
+        return <ListItem
           key={index}
           leftIcon={
             <div
-              style={{ display: 'flex' }}
               className="instructionCounter"
+              style={{ display: 'flex' }}
             >
               <div>{index + 1}</div>
             </div>}
           primaryText={instruction}
         />
-      )}
+      })}
     </List>;
   }
 });
