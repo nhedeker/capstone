6223f8b5d99cb2c7af102841c2cd6a4689af2cbe
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-09-15 16:37:12 -0700

    Refactored Styling of NotFound Component

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -1,4 +1,5 @@
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import { Link } from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
 
@@ -8,11 +9,13 @@ const NotFound = React.createClass({
       <h1>Uh Oh!</h1>
       <h2>The page you were looking for doesn't exist.</h2>
       <img className="errorImg" src="./PusheenEats.png" />
-      <RaisedButton
-        icon={<ArrowBack />}
-        label="Return to Homepage"
-        primary={true}
-      />
+      <Link to="/">
+        <RaisedButton
+          icon={<ArrowBack />}
+          label="Return to Homepage"
+          primary={true}
+        />
+      </Link>
     </div>;
   }
 });
